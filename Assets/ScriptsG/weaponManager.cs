0e02069ba3607d2b723bcf102853ea100afd2cfd
Fullmetalgaz <garethj.evans750@gmail.com>
2017-11-27 16:06:56 +0000

    Commit with code
    Commit with starting example code, assets and so on. Includes destructible scripts and objects as well as player controller

---
+++ Assets/ScriptsG/weaponManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weaponManager : MonoBehaviour {
+    public GameObject Cube_cell;
+
+    void OnMouseDown()
+    {
+        if (Cube_cell)
+        {
+            Destroy(gameObject);
+        }   
+    }
+}
+
