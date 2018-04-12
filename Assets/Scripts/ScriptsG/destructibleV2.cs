fc1e0a41feab5c920a3101684a35d6c358e26fc3
Fullmetalgaz <garethj.evans750@gmail.com>
2018-04-12 22:50:16 +0100

    Edits to destructible and throw
    minor edits to destructible and throw scripts

--- Assets/Scripts/ScriptsG/destructibleV2.cs
+++ Assets/Scripts/ScriptsG/destructibleV2.cs
@@ -6,26 +6,30 @@ public class destructibleV2 : MonoBehaviour
 {
 
     public GameObject debrisPrefab; //prefab reference for broken object
-    public int objectHealth; //Health of object
+    public float objectHealth; //Health of object
     public ThrowObjectG incomingDmg; //damage value taken from throw object taken from the incoming object
     public GameObject incomingObject; //bring reference for object that is going to collide
     public Rigidbody rb;  //reference for rigidbody to use with ThrowObjectG.throwforce
     float collisionTime = 0;
     private float cooldown = 0.1f;
 
-
-    void OnCollisionEnter(Collision collision)
+    void CollisionTimer()
     {
-        Debug.Log("Time");
-        Debug.Log(Time.time);
         collisionTime += Time.deltaTime;
         if (collisionTime > cooldown)
         {
             return;
         }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Time");
+        Debug.Log(Time.time);
+
+        CollisionTimer();
         if (collision.gameObject.tag == "PhysicsObject") // will look for the incoming Physics Object before carrying on with steps
         {
-            //collisionTime = Time.time + cooldown;
             GameObject incomingObject = collision.gameObject; //finds the incoming object
             ThrowObjectG incomingDmg = incomingObject.GetComponent<ThrowObjectG>();
             rb = incomingObject.gameObject.GetComponent<BoxCollider>().attachedRigidbody;
@@ -33,12 +37,12 @@ public class destructibleV2 : MonoBehaviour
             Debug.Log("Before Health");
             Debug.Log(objectHealth);
 
-            objectHealth = objectHealth - incomingDmg.dmg; //takes object health and will subtract the total from incoming damage. Should help with balancing object's damage when breaking
+            objectHealth = objectHealth - incomingDmg.totaldmg; //takes object health and will subtract the total from incoming damage. Should help with balancing object's damage when breaking
                                                            //objects and damaging players
             Debug.Log("After Health");
             Debug.Log(objectHealth);
 
-            if (objectHealth == 0) //when object is dead
+            if (objectHealth <= 0) //when object is dead
             {
                 if (debrisPrefab)
                 {
